b5e307b2d389d57a0fb5d416a598ab0ef55fe685
himani23724 <himanirouniyar407@gmail.com>
2025-06-22 04:19:39 +0530

    Update program.cs

--- factorymethodpatternexample/program.cs
+++ factorymethodpatternexample/program.cs
@@ -1,1 +1,21 @@
 
+﻿using System;
+
+namespace FactoryMethodPatternExample
+{
+   
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Factory Method Pattern Example in C#");
+            Console.WriteLine("====================================\n");
+            
+            // Run the factory method tests
+            FactoryMethodTest.TestFactoryMethod();
+            
+            Console.WriteLine("\nPress any key to exit...");
+            Console.ReadKey();
+        }
+    }
+}
