ede6d83d5bd5a8edcfd50a564862dd5b6c801612
himani23724 <himanirouniyar407@gmail.com>
2025-06-22 04:49:07 +0530

    Update worddocumentfactory.cs

--- 1_1_DPP/FactoryMethodPatternExample/factories/worddocumentfactory.cs
+++ 1_1_DPP/FactoryMethodPatternExample/factories/worddocumentfactory.cs
@@ -1,1 +1,14 @@
+using System;
 
+namespace FactoryMethodPatternExample
+{
+    
+    public class WordDocumentFactory : DocumentFactory
+    {
+        public override Document CreateDocument(string name)
+        {
+            Console.WriteLine("WordDocumentFactory: Creating Word document...");
+            return new WordDocument(name);
+        }
+    }
+}
