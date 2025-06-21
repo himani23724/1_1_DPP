b31d99ca73c0517ffb180dc7f4c21fe79d2f9bf4
himani23724 <himanirouniyar407@gmail.com>
2025-06-22 04:43:54 +0530

    Update worddocument.cs

--- 1_1_DPP/FactoryMethodPatternExample/documents/worddocument.cs
+++ 1_1_DPP/FactoryMethodPatternExample/documents/worddocument.cs
@@ -1,1 +1,46 @@
+using System;
 
+namespace FactoryMethodPatternExample
+{
+    
+    public class WordDocument : Document
+    {
+        public int WordCount { get; private set; }
+        
+        public WordDocument(string name) : base(name)
+        {
+            FileExtension = ".docx";
+            WordCount = 0;
+        }
+        
+        public override void Open()
+        {
+            Console.WriteLine($"Opening Word document: {Name}{FileExtension}");
+            Console.WriteLine("Microsoft Word application launched");
+        }
+        
+        public override void Save()
+        {
+            Console.WriteLine($"Saving Word document: {Name}{FileExtension}");
+            Console.WriteLine("Document saved in Word format");
+        }
+        
+        public override void Close()
+        {
+            Console.WriteLine($"Closing Word document: {Name}{FileExtension}");
+            Console.WriteLine("Word application closed");
+        }
+        
+        public override void Print()
+        {
+            Console.WriteLine($"Printing Word document: {Name}{FileExtension}");
+            Console.WriteLine("Sending to default printer...");
+        }
+        
+        public void AddText(string text)
+        {
+            WordCount += text.Split(' ').Length;
+            Console.WriteLine($"Added text to document. Current word count: {WordCount}");
+        }
+    }
+}
