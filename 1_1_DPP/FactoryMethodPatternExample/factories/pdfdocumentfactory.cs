8bde122328a017eb2e612e8c578cc65354fade42
himani23724 <himanirouniyar407@gmail.com>
2025-06-22 04:47:45 +0530

    Update pdfdocumentfactory.cs

--- 1_1_DPP/FactoryMethodPatternExample/factories/pdfdocumentfactory.cs
+++ 1_1_DPP/FactoryMethodPatternExample/factories/pdfdocumentfactory.cs
@@ -1,1 +1,14 @@
+using System;
 
+namespace FactoryMethodPatternExample
+{
+ 
+    public class PdfDocumentFactory : DocumentFactory
+    {
+        public override Document CreateDocument(string name)
+        {
+            Console.WriteLine("PdfDocumentFactory: Creating PDF document...");
+            return new PdfDocument(name);
+        }
+    }
+}
