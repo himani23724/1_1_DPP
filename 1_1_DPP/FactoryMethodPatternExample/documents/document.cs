a4c6752a229765e08f0791638582e9204b5f1e89
himani23724 <himanirouniyar407@gmail.com>
2025-06-22 04:40:39 +0530

    Update document.cs

--- 1_1_DPP/FactoryMethodPatternExample/documents/document.cs
+++ 1_1_DPP/FactoryMethodPatternExample/documents/document.cs
@@ -1,1 +1,34 @@
+using System;
 
+namespace FactoryMethodPatternExample
+{
+    /// <summary>
+    /// Abstract base class defining the common interface for all documents
+    /// </summary>
+    public abstract class Document
+    {
+        public string Name { get; protected set; }
+        public string FileExtension { get; protected set; }
+        public DateTime CreatedDate { get; protected set; }
+        
+        protected Document(string name)
+        {
+            Name = name;
+            CreatedDate = DateTime.Now;
+        }
+        
+        // Abstract methods that concrete documents must implement
+        public abstract void Open();
+        public abstract void Save();
+        public abstract void Close();
+        public abstract void Print();
+        
+        // Common method for all documents
+        public virtual void DisplayInfo()
+        {
+            Console.WriteLine($"Document: {Name}{FileExtension}");
+            Console.WriteLine($"Created: {CreatedDate:yyyy-MM-dd HH:mm:ss}");
+            Console.WriteLine($"Type: {GetType().Name}");
+        }
+    }
+}
