a20d627708c2b29f4334d7f943b06215a7829b3f
himani23724 <himanirouniyar407@gmail.com>
2025-06-22 04:46:36 +0530

    Update exceldocumentfactory.cs

--- 1_1_DPP/FactoryMethodPatternExample/factories/exceldocumentfactory.cs
+++ 1_1_DPP/FactoryMethodPatternExample/factories/exceldocumentfactory.cs
@@ -1,1 +1,14 @@
+using System;
 
+namespace FactoryMethodPatternExample
+{
+    
+    public class ExcelDocumentFactory : DocumentFactory
+    {
+        public override Document CreateDocument(string name)
+        {
+            Console.WriteLine("ExcelDocumentFactory: Creating Excel document...");
+            return new ExcelDocument(name);
+        }
+    }
+}
