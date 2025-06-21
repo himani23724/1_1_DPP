920bb6eca14ac35ee01ed12a0e29fffd858cdc41
himani23724 <himanirouniyar407@gmail.com>
2025-06-22 04:45:38 +0530

    Update documentfactory.cs

--- 1_1_DPP/FactoryMethodPatternExample/factories/documentfactory.cs
+++ 1_1_DPP/FactoryMethodPatternExample/factories/documentfactory.cs
@@ -1,1 +1,24 @@
+using System;
 
+namespace FactoryMethodPatternExample
+{
+    
+    public abstract class DocumentFactory
+    {
+        // Factory method - to be implemented by concrete factories
+        public abstract Document CreateDocument(string name);
+        
+        // Template method that uses the factory method
+        public Document ProcessDocument(string name)
+        {
+            Console.WriteLine($"\n--- Processing document creation for: {name} ---");
+            Document document = CreateDocument(name);
+            
+            // Common processing steps
+            document.DisplayInfo();
+            document.Open();
+            
+            return document;
+        }
+    }
+}
